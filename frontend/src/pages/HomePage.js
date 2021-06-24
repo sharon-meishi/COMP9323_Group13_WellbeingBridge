4e34eca22918de09c682b47aa1e76a73dc79988e
taosha <tinghu912@gmail.com>
2021-06-24 15:49:27 +0800

    EventCardInit

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React from 'react';
+import EventCard from '../components/EventCard';
 function HomePage() {
     return (
         <div>
             Homepage
+            <EventCard></EventCard>
         </div>
     )
 }
