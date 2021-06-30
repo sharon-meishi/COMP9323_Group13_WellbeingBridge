edbf97ccf3bd0d2144bc34524d380bb9f302ade2
sharon-meishi <55526336+sharon-meishi@users.noreply.github.com>
2021-06-30 17:54:39 +1000

    cardborad update

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 // import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -17,111 +17,113 @@ import ShareIcon from '@material-ui/icons/Share';
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 // import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SamplePic from '../Assets/eventPic.jpeg';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      width: 325,
-      margin:'20px 0 20px 0',
-    },
-    media: {
-      height: 0,
-      paddingTop: '85%', // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    title:{
-      fontSize:"1.2rem",
-      // paddingRight:"1%",
-      width:"65%",
-      textDecoration:"underline",
-      // color:"textSecondary",
-    },
-    location:{
-      fontSize:"0.9rem",
-      justifyContent:"end",
-      paddingTop:"9%",
-      width:"35%",
-      // color:"textSecondary",
-
-    },
-    // expandOpen: {
-    //   transform: 'rotate(180deg)',
-    // },
-    date:{
-      fontSize:"0.7rem",
-    },
-    detail:{
-      height:"10%",
-    },
-    // avatar: {
-    //   backgroundColor: red[500],
-    // },
-    actions:{
-      display:"flex",
-    },
-    view:{
-      paddingLeft:"30%",
-      color:"#3a89c2",
-      textDecoration:"underline",
-
-    }
-  }));
+  root: {
+    width: 325,
+    margin: '20px 0 20px 0',
+  },
+  media: {
+    height: 0,
+    paddingTop: '85%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  title: {
+    fontSize: '1.2rem',
+    // paddingRight:"1%",
+    width: '65%',
+    textDecoration: 'underline',
+    // color:"textSecondary",
+  },
+  location: {
+    fontSize: '0.9rem',
+    justifyContent: 'end',
+    paddingTop: '9%',
+    width: '35%',
+    // color:"textSecondary",
+  },
+  // expandOpen: {
+  //   transform: 'rotate(180deg)',
+  // },
+  date: {
+    paddingTop: '2%',
+    fontSize: '0.7rem',
+  },
+  detail: {
+    paddingTop: '2%',
+    height: '10%',
+  },
+  // avatar: {
+  //   backgroundColor: red[500],
+  // },
+  actions: {
+    display: 'flex',
+  },
+  view: {
+    paddingLeft: '35%',
+  },
+}));
 function EventCard(props) {
-    const classes = useStyles();
-    // const [expanded, setExpanded] = React.useState(false);
-    const {eventId, thumbnail, name, date, location, favourite} = props.info;
-    // const handleExpandClick = () => {
-    //   setExpanded(!expanded);
-    // };
-    // console.log(id);
-    console.log(props);
-    // console.log(props.info);
+  const classes = useStyles();
+  // const [expanded, setExpanded] = React.useState(false);
+  const { eventId, thumbnail, name, date, location, favourite } = props.info;
+  const preventDefault = (event) => event.preventDefault();
+  // const handleExpandClick = () => {
+  //   setExpanded(!expanded);
+  // };
+  // console.log(id);
+  console.log(props);
+  // console.log(props.info);
 
-    return (
-      <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image={SamplePic}
-          title="Paella dish"
-        />
-        <CardContent>
-          <Grid container direction="row">
-            <Typography className={classes.title}  >
-              {name}
-            </Typography>          
-            <Typography className={classes.location}  >
-              {location.suburb}
-            </Typography>
-          </Grid>
-          <Typography  className={classes.date} color="textSecondary">
-            29, June 2021
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={SamplePic}
+        title='Paella dish'
+      />
+      <CardContent>
+        <Grid container direction='row'>
+          <Typography className={classes.title}>{name}</Typography>
+          <Typography className={classes.location}>
+            {location.suburb}
           </Typography>
-          <Grid className={classes.detail}>
-            <Typography   color="textSecondary">
-              Aimed at all levels of fitness and age groups, the classes will be gentle...
-            </Typography>
-          </Grid>
-        </CardContent>
-        <CardActions className={classes.actions} disableSpacing className={classes.actions}>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          <Typography className={classes.view}>
-            view Details
+        </Grid>
+        <Typography className={classes.date} color='textSecondary'>
+          29, June 2021
+        </Typography>
+        <Grid className={classes.detail}>
+          <Typography >
+            Aimed at all levels of fitness and age groups, the classes will be
+            gentle exercise focus.
           </Typography>
-        </CardActions>
-      </Card>
-    );
+        </Grid>
+      </CardContent>
+      <CardActions className={classes.actions} disableSpacing>
+        <IconButton aria-label='add to favorites'>
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label='share'>
+          <ShareIcon />
+        </IconButton>
+        <Link
+          href='#'
+          onClick={preventDefault}
+          className={classes.view}
+          variant='body2'
+        >
+          Discover More
+        </Link>
+      </CardActions>
+    </Card>
+  );
 }
 
-export default EventCard
-
-
+export default EventCard;
--- frontend/src/components/HomePage/CardBoard.js
+++ frontend/src/components/HomePage/CardBoard.js
@@ -3,99 +3,123 @@ import { makeStyles } from '@material-ui/core/styles';
 import EventCard from '../EventCard';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-// import Box from '@material-ui/core/Box';
+import HomePageButton from '../HomePageButton';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
   root: {
     // backgroundColor:'#C5EDF9',
     // justifyContent:'center',
-    direction:"row",
-    justifyContent:"space-around ",
-    alignItems:"space-between",
-    padding:'0 10% 0 10%',
+    direction: 'row',
+    justifyContent: 'space-around ',
+    alignItems: 'space-between',
+    padding: '0 20% 0 20%',
   },
-  text:{
+  text: {
     // justifyContent:'space-between',
     // backgroundColor:'black',
     // margin: '20px',
-    textDecoration:'underline',
-    padding:'1% 13% 0 13%',
+    textDecoration: 'underline',
+    padding: '1% 0 0 0',
+  },
+  title:{
+    width: '70%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '16px',
   }
-
 });
 const CardBoard = () => {
   const classes = useStyles();
-  const event_list = [{
-    "eventId": 1,
-    "thumbnail": "image link",
-    "name": "Community Yoga Class",
-    "date": "",
-    "location": {
-      "postcode": "2131",
-      "suburb": "Ashfield NSW"
+  const event_list = [
+    {
+      eventId: 1,
+      thumbnail: 'image link',
+      name: 'Community Yoga Class',
+      date: '',
+      location: {
+        postcode: '2131',
+        suburb: 'Ashfield NSW',
+      },
+      favourite: true,
     },
-    "favourite": true
-  },{
-    "eventId": 2,
-    "thumbnail": "image link",
-    "name": "Community Yoga Class",
-    "date": "",
-    "location": {
-      "postcode": "2131",
-      "suburb": "Ashfield NSW"
+    {
+      eventId: 2,
+      thumbnail: 'image link',
+      name: 'Community Yoga Class',
+      date: '',
+      location: {
+        postcode: '2131',
+        suburb: 'Ashfield NSW',
+      },
+      favourite: true,
     },
-    "favourite": true
-  },{
-    "eventId": 3,
-    "thumbnail": "image link",
-    "name": "Community Yoga Class",
-    "date": "",
-    "location": {
-      "postcode": "2131",
-      "suburb": "Ashfield NSW"
+    {
+      eventId: 3,
+      thumbnail: 'image link',
+      name: 'Community Yoga Class',
+      date: '',
+      location: {
+        postcode: '2131',
+        suburb: 'Ashfield NSW',
+      },
+      favourite: true,
     },
-    "favourite": true
-  },{
-    "eventId": 4,
-    "thumbnail": "image link",
-    "name": "Community Yoga Class",
-    "date": "",
-    "location": {
-      "postcode": "2131",
-      "suburb": "Ashfield NSW"
+    {
+      eventId: 4,
+      thumbnail: 'image link',
+      name: 'Community Yoga Class',
+      date: '',
+      location: {
+        postcode: '2131',
+        suburb: 'Ashfield NSW',
+      },
+      favourite: true,
     },
-    "favourite": true
-  },{
-    "eventId": 5,
-    "thumbnail": "image link",
-    "name": "Community Yoga Class",
-    "date": "",
-    "location": {
-      "postcode": "2131",
-      "suburb": "Ashfield NSW"
+    {
+      eventId: 5,
+      thumbnail: 'image link',
+      name: 'Community Yoga Class',
+      date: '',
+      location: {
+        postcode: '2131',
+        suburb: 'Ashfield NSW',
+      },
+      favourite: true,
     },
-    "favourite": true
-  },{
-    "eventId": 6,
-    "thumbnail": "image link",
-    "name": "Community Yoga Class",
-    "date": "",
-    "location": {
-      "postcode": "2131",
-      "suburb": "Ashfield NSW"
+    {
+      eventId: 6,
+      thumbnail: 'image link',
+      name: 'Community Yoga Class',
+      date: '',
+      location: {
+        postcode: '2131',
+        suburb: 'Ashfield NSW',
+      },
+      favourite: true,
     },
-    "favourite": true
-  }];
+  ];
   // const event_board = event_list.map(event => <EventCard key={event.eventId} className={classes.item}></EventCard>);
   return (
-    <div>
-      <Typography variant="h4" className={classes.text}>What's on</Typography>
+    <Box display='flex' flexDirection='column' alignItems='center' justifyContent='space-between'>
+      <Box className={classes.title}>
+        <Typography variant='h4'  className={classes.text}>
+          What's on
+        </Typography>
+        <HomePageButton text='Find an event' />
+      </Box>
       <Grid container className={classes.root}>
-        {event_list.map(event => <EventCard key={event.eventId} className={classes.item} info={event}></EventCard>)}
+        {event_list.map((event) => (
+          <EventCard
+            key={event.eventId}
+            className={classes.item}
+            info={event}
+          ></EventCard>
+        ))}
       </Grid>
-
-    </div>
-  )
-}
+    </Box>
+  );
+};
 
 export default CardBoard;
