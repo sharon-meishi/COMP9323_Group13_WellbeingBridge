032a168a83a203ccf808c0c34aa90dac9b1ca272
Jie Peng <jiepeng@Jies-MacBook-Pro.local>
2021-07-09 22:38:48 +1000

    add insert sql

--- backend/App.py
+++ backend/App.py
@@ -40,38 +40,104 @@ def create_database():
     '''
     organization_table = '''CREATE TABLE IF NOT EXISTS `Organization` (
   `OrganizationId` int NOT NULL AUTO_INCREMENT,
-  `Email` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `OrganizationName` varchar(255) NOT NULL,
-  `OrganizationType` varchar(255) NOT NULL,
-  `Contact` varchar(255) NOT NULL,
-  `Introduction` varchar(255) DEFAULT NULL,
-  `Logo` varchar(255) DEFAULT NULL,
-  `VideoUrl` varchar(255) DEFAULT NULL,
-  `ServiceList` varchar(255) DEFAULT NULL,
-  `WebsiteLink` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`OrganizationId`))
-    '''
+  `Email` varchar(256) NOT NULL,
+  `Password` varchar(256) NOT NULL,
+  `OrganizationName` varchar(256) NOT NULL,
+  `OrganizationType` varchar(256) NOT NULL,
+  `Logo` TEXT DEFAULT NULL,
+  `Contact` varchar(256) NOT NULL,
+  `Introduction` TEXT DEFAULT NULL,
+  `Details` TEXT DEFAULT NULL,
+  `VideoUrl` varchar(256) DEFAULT NULL,
+  `ServiceList` TEXT DEFAULT NULL,
+  `WebsiteLink` TEXT DEFAULT NULL,
+  `otherEvents` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`OrganizationId`));'''
+
+    insert_organization='''INSERT INTO Organization (OrganizationId,Email,Password,OrganizationName,OrganizationType,
+    Logo,Contact,Introduction,Details,VideoUrl,ServiceList,WebsiteLink,otherEvents) 
+    VALUES (0, "wellbeing@org.com", 
+    "abcd", "HeadSpace", "Youth","https://sydneynorthhealthnetwork.org.au/wp-content/uploads/2016/04/Headspace-logo
+    .png","headspace@newhorizons.net.au","The Centre is open to assist young people with health advice and support 
+    and information, around a range of matters including: caring for others, stress, relationships, employment and 
+    depression","Headspace is the National Youth Mental Health Foundation providing early intervention mental health 
+    services to 12-25 year olds, along with assistance in promoting young peoples’ wellbeing.",
+    "https://youtu.be/DxIDKZHW3-E",'"mental health counselling","education support","employment support","alcohol and 
+    other drug services"', "https://headspace.org.au","1,2,3"), 
+    (1, 'test@org.com', 'abcd', "My Aged Care", "Seniors", 
+    "https://www.myagedcare.gov.au/themes/custom/myagedcare/logo.png", "1800-200-422", "My Aged Care is the 
+    Australian Government's starting point on your aged care journey. Find and access the government-funded services 
+    you need.", "My Aged Care will offer: prompt, reliable and confidential services; polite, helpful and 
+    knowledgeable staff; clear information, available in other languages if you speak another language; support to 
+    access information if you have hearing difficulties or a vision impairment; help to find government funded aged 
+    care services; prompt resolution of any complaint or concern you have with My Aged Care.", 
+    "https://youtu.be/QkWMK7gDVkw", '"Information on the different types of aged care services available", 
+    "An assessment of needs to identify eligibility and the right type of care", "Referrals and support to find 
+    service providers that can meet your needs", "Information on what you might need to pay towards the cost of your 
+    care."', "https://www.myagedcare.gov.au", '1,2,3'), 
+    (2, '9323@org.com', 'abcd', "House to Grow", "Education", 
+    "https://brizy.b-cdn.net/media/iW=70&iH=80&oX=0&oY=0&cW=70&cH=80/d1073b4eea3ad956e7750133d6091c22.png", 
+    "info@housetogrow.org", "Empowering communities through education for life, holistic health and wellbeing ", 
+    "House to Grow is a not-for-profit organisation that empowers vulnerable children in unhealthy environments and 
+    women affected by domestic violence through personal development, education for life and holistic health. We try 
+    not to limit who can come to us, we encourage and support any individual that seeks personal development, 
+    education for life and holistic health. We also deliver educational programs to organisations, community groups 
+    and people in need. ", "video link", '"Colouring Dreams - Creating an environment for children to dream", 
+    "Growing Healthy International Students - More Self improvement for better societies", "The Flight of the 
+    Butterflies - Empowering women", "Building Emotionally Intelligent Communities - Get your volunteers ready for 
+    tomorrow’s job"', "https://www.housetogrow.org", '1,2,3'); '''
     event_table = '''
     CREATE TABLE IF NOT EXISTS `Event` (
-  `EventId` int NOT NULL,
+  `EventId` int NOT NULL AUTO_INCREMENT,
+  `EventName` varchar(255) NOT NULL,
   `OrganizationId` int NOT NULL,
   `OrganizationName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `EventName` varchar(255) NOT NULL,
   `Thumbnail` varchar(255) NOT NULL,
+  `Format` varchar(255) NOT NULL,
   `Postcode` varchar(255) NOT NULL,
   `Suburb` varchar(255) NOT NULL,
   `Street` varchar(255) NOT NULL,
   `venue` varchar(255) NOT NULL,
-  `Date` date NOT NULL,
-  `Time` datetime NOT NULL,
-  `Details` varchar(1000) NOT NULL,
-  `Recommendation` varchar(1000) DEFAULT NULL,
-  `Favourites` int DEFAULT NULL,
-  `Introduction` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`EventId`)
-)
+  `Date` varchar(255) NOT NULL,
+  `Time` varchar(255) NOT NULL,
+  `Introduction` TEXT DEFAULT NULL,
+  `Details` TEXT DEFAULT NULL,
+  `Recommendation` TEXT DEFAULT NULL,
+  `BookedUser` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`EventId`))
     '''
+
+    insert_event = '''
+    INSERT INTO Event
+  (EventId,EventName,OrganizationId,OrganizationName,Thumbnail,Format,Postcode,Suburb,Street,
+  venue,Date,Time,Introduction,Details,Recommendation,BookedUser)
+VALUES(0,"Kids Yoga Class",2,"Innerwest Council" ,
+"https://www.indianyoga.school/public/uploads/gallery/kids-yoga-ttc.jpg","Class",
+2131,"Ashfield NSW","Parramatta Road and Orpington Street","Ashfield Park",
+"14-07-2021",
+"11:00 AM to 12:00 PM",
+ "Aimed at kids between 5 to 12, the classes will be gentle exercise focus, incorporating the local landscape and body weight exercises during the 60 minute session. ",
+ "Kids must attend with parents. All you need is your yoga mat, a workout towel, a water bottle and a sense of fun!",
+'1,2,3','1,2,3'),
+
+(1,"Tai Chi for seniors",2,"My Aged Care" ,
+"https://www.greatseniorliving.com/assets/img/tai-chi-for-seniors-pin-@1X.jpg","Class",
+2039,"Rozelle NSW","608 Darling Street","Hannaford Community Centre",
+"20-07-2021","10:00 AM to 11:00 AM",
+ "Traditional Yang Style Tai Chi for seniors with particular focus on the 85 forms.",
+ "Health benefits are derived from the Tai Chi's slow, gentle and tranquil movements which enable harmony in mind and body, improved mobility, suppleness and mental alertness. Bookings essential. ",
+'1,2,3','1,2,3'),
+
+(2,"Youth - #TrueDreamersTour2021",3,"House to Grow" ,
+"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F137085143%2F307439946789%2F1%2Foriginal.20210531-025831?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=1%2C78%2C1250%2C625&s=3d6e67be19885a5a9411e28685a3bf0a",
+"Class",
+2204,"Marrickville NSW","142 Addison Road","142 Addison Rd","01-08-2021","11:00 AM to 3:00 PM",
+ "A half day of activity that challenges young people to engage with wellbeing and make new connections in a friendly and fun environment.",
+ "# TrueDreamersTour2021 event promises a half day of activity that challenges young people to engage with health and make new connections in a friendly and fun environment. including
+              Inspirational stories from internaitonal students; Documentary internaional students in Australia; Round table with experts in health and education; Yoga and meditation activity; Live Music; Gift vouchers",
+'1,2,3','1,2,3');
+    '''
+
     booking_table = '''
     CREATE TABLE IF NOT EXISTS `Booking` (
   `BookingId` int NOT NULL AUTO_INCREMENT,
@@ -82,8 +148,13 @@ def create_database():
     '''
     c.execute(user_table)
     c.execute(organization_table)
+
     c.execute(event_table)
     c.execute(booking_table)
+    c.execute(insert_organization)
+    db.commit()
+    c.execute(insert_event)
+    db.commit()
     c.close()
 
     return True
@@ -139,13 +210,13 @@ class IndividualRegister(Resource):
                 sql = "INSERT INTO User VALUES ({},'{}', '{}', '{}', NULL);".format(userid, nickname, email, password)
                 sql_command(sql)
                 select_sql = f"SELECT UserId FROM User WHERE Email='{email}';"
-                id=sql_command(select_sql)[0][0]
-                token=encode_token(email,"individual")
+                id = sql_command(select_sql)[0][0]
+                token = encode_token(email, "individual")
                 output = {
                     "message": "Success register",
                     "nickname": nickname,
-                    "userid":id,
-                    "token":token
+                    "userid": id,
+                    "token": token
                 }
                 return output, 200
 
@@ -191,13 +262,13 @@ class OrganizationRegister(Resource):
                            introduction)
                 sql_command(sql)
                 select_sql = f"SELECT OrganizationId FROM Organization WHERE Email='{email}';"
-                id=sql_command(select_sql)[0][0]
-                token=encode_token(email,"organization")
+                id = sql_command(select_sql)[0][0]
+                token = encode_token(email, "organization")
                 output = {
                     "message": "Success register",
-                    "email":email,
-                    "organizationid":id,
-                    "token":token
+                    "email": email,
+                    "organizationid": id,
+                    "token": token
                 }
                 return output, 200
 
@@ -294,21 +365,171 @@ class event(Resource):
         if result:
             # location = {"postcode": result[0][4], "suburb": result[0][5]}
             result_output = {"eventId": result[0][0],
-                                 "thumbnail": result[0][1],
-                                 "name": result[0][2],
-                                 "date": result[0][3],
-                                 "location": {
-                                     "postcode": result[0][4],
-                                     "suburb": result[0][5]
-                                 },
-                                 "introduction": result[0][6],
-                                 "favourite": favourite}
+                             "thumbnail": result[0][1],
+                             "name": result[0][2],
+                             "date": result[0][3],
+                             "location": {
+                                 "postcode": result[0][4],
+                                 "suburb": result[0][5]
+                             },
+                             "introduction": result[0][6],
+                             "favourite": favourite}
             return result_output, 200
         else:
             output = {
-                    "message": "Not Found"
-                }
+                "message": "Not Found"
+            }
             return output, 404
+
+
+location_model = api.model("location", {
+    "postcode": fields.String,
+    "suburb": fields.String,
+    "street": fields.String,
+    "venue": fields.String
+})
+event_model = api.model("event", {
+    "eventName": fields.String,
+    "thumbnail": fields.String,
+    "format": fields.String,
+    "location": fields.Nested(location_model),
+    "date": fields.String,
+    "time": fields.String,
+    "introduction": fields.String,
+    "details": fields.String
+})
+
+token_parser = api.parser()
+token_parser.add_argument('token', type=str)
+
+
+@api.route("/event", doc={"description": "publish details of an event"})
+@api.doc(parser=token_parser)
+class PublishEvent(Resource):
+    @api.expect(event_model)
+    def post(self):
+        print("ok")
+        data = json.loads(request.get_data())
+        print(data)
+        token = token_parser.parse_args()['token']
+        if token is None:
+            output = {
+                "message": "Invalid input"
+            }
+            return output, 400
+        user_info = decode_token(token)
+        user_type = user_info['type']
+        print(user_type)
+        if user_type != 'organization':
+            output = {
+                "message": "Cannot access.Wrong token!"
+            }
+            return output, 403
+        else:
+            org_email = user_info['email']
+            org_sql = f"SELECT OrganizationId,OrganizationName FROM Organization WHERE Email = '{org_email}';"
+            org_result = sql_command(org_sql)[0]
+            sql = "INSERT INTO Event VALUES (0,'{}', {}, '{}', '{}','{}','{}','{}','{}','{}','{}','{}','{}','{}'," \
+                  "NULL,NULL);". \
+                format(data['eventName'], org_result[0], org_result[1], data['thumbnail'], data['format'],
+                       data['location']['postcode'], data['location']['suburb'],
+                       data['location']['street'], data['location']['venue'], data['date'], data['time'],
+                       data['introduction'], data['details'])
+            print(sql)
+            sql_command(sql)
+            output = {
+                "message": "success"
+            }
+        return output, 200
+
+
+@api.route("/event/{eventid}", doc={"description": "get details of an event"})
+@api.doc(parser=parser)
+class GetEventbyId(Resource):
+    def get(self):
+        token = parser.parse_args()['token']
+        eventid = parser.parse_args()['eventid']
+        event_sql = f"SELECT * FROM Event WHERE EventId={eventid};"
+        event_info = sql_command(event_sql)[0]
+        if token is None:
+            booked = False
+            favourite = False
+        else:
+            email = decode_token(token)['email']
+            user_sql = f"SELECT Userid FROM User WHERE Email='{email}';"
+            userid = sql_command(user_sql)[0][0]
+            if str(userid) in event_info[15].split(","):
+                booked = True
+            else:
+                booked = False
+            user_sql = f"SELECT FavouriteId FROM User WHERE Email='{email}';"
+            if_favourite = sql_command(user_sql)
+            favouriteid = list(if_favourite[0])[0]
+            if str(eventid) in favouriteid.split(","):
+                favourite = True
+            else:
+                favourite = False
+        comment_sql = f"SELECT * FROM Comment WHERE eventid={eventid};"
+        comments = []
+
+        comments_result = sql_command(comment_sql)
+        for data in comments_result:
+            comment_temp = {}
+            comment_temp['commentId'] = data[0]
+            comment_temp['userId'] = data[1]
+            comment_temp['username'] = data[2]
+            comment_temp['published'] = str(data[5])
+            comment_temp['comment'] = data[4]
+            # comment_temp = json.dumps(comment_temp)
+            comments.append(comment_temp)
+        output = {
+            "eventId": eventid,
+            "eventName": event_info[1],
+            "OrganizationId": event_info[2],
+            "OrganizationName": event_info[3],
+            "thumbnail": event_info[4],
+            "format": event_info[5],
+            "location": {
+                "postcode": event_info[6],
+                "suburb": event_info[7],
+                "street": event_info[8],
+                "venue": event_info[9],
+            },
+            "date": event_info[10],
+            "time": event_info[11],
+            "introduction": event_info[12],
+            "details": event_info[13],
+            "comments": comments,
+            "recommendation": list(map(int, event_info[14].split(","))),
+            "bookedUser": list(map(int, event_info[15].split(","))),
+            "booked": booked,
+            "favourite": favourite
+        }
+        return output
+
+    @api.expect(event_model)
+    def put(self):
+        data = api.payload
+        token = parser.parse_args()['token']
+        eventid = parser.parse_args()['eventid']
+        if token is None:
+            output = {
+                "message": "bad request!"
+            }
+            return output, 405
+        else:
+            user_type = decode_token(token)['type']
+            if user_type != 'organization':
+                output = {
+                    "message": "wrong token!"
+                }
+                return output, 403
+            update_sql = f"UPDATE Event SET EventName='{data['eventName']}', Thumbnail='{data['thumbnail']}',Format='{data['format']}',Postcode='{data['location']['postcode']}',Suburb='{data['location']['suburb']}',Street='{data['location']['street']}',venue='{data['location']['venue']}',Date='{data['date']}',Time='{data['time']}',Introduction='{data['introduction']}',Details='{data['details']}' WHERE Eventid={eventid};"
+            sql_command(update_sql)
+            output = {
+                "message": "Success"
+            }
+        return output, 200
 
 
 if __name__ == "__main__":
