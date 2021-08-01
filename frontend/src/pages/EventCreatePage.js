ea52baef8f9d75dcbc8118f093f10296b1c11a62
Tim-hyx <123056696@qq.com>
2021-08-01 14:20:56 +0800

    test

--- frontend/src/pages/EventCreatePage.js
+++ frontend/src/pages/EventCreatePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../components/NavigationBar/NavBar';
 import BackToTop from '../components/BackToTop';
-import EventForm from '../components/EventEditPage/EventForm';
+import EventForm from '../components/EventEditPage/EventForm';//sdsd
 
 function EventCreatePage() {
   return (
