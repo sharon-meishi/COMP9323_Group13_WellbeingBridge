cc04a0c4cf159c26456a3cabbd29ec1384bcd6cc
sharon-meishi <meishichen1106@gmail.com>
2021-08-09 01:21:55 +1000

    Add config.py for configuration

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+# MySQL database
+DB_ACCOUNT = 'root'
+DB_PASSWORD = 'unsw1234'
+DB_NAME = 'wellbeing'
+DB_URL = 'localhost'
+DB_PORT = 3306
+
+# SMTP email
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
+EMAIL_ADDRESS = 'wellbeingbridge@gmail.com'
+EMAIL_PASSWORD = 'unsw1234'
